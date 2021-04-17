7ed80f576811f4643530fa783e27f812b144e45d
Tapani Moilanen <moilanen.tapani@gmail.com>
2021-04-18 00:44:58 +0300

    Add more dotenv stuff

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # C8bot
 Discord botti. MVP announcementtii ja muut random paskaa
+
+## How to run
+
+* requires Node 14.x
+* copy `.env.dist` to `.env` and add required values
+
+```bash
+npm install
+
+npm run dev
+```
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
+import _ from './env';
 import Discord from "discord.js";
+
 const client = new Discord.Client();
-client.login("ODMzMDY3OTM2NTY2MzQ1Nzcw.YHs8-Q.SKmcEufh0jqWGXS1GkKu1pMhqBE");
 
-client.on("ready", () => {
-    console.log("Bot ready!");
-  });
+async function main() {
+  if (!process.env.DISCORD_TOKEN) {
+    throw new Error('No discord token configured, copy .env.dist to .env and set DISCROD_TOKEN')
+  }
 
-  client.on("message", (message) => {
-    if (message.content === "!test") {
-      message.reply("Toimii");
-    }
-  });
+  // Login to discord
+  await client.login(process.env.DISCORD_TOKEN);
+
+  client.on("ready", () => {
+      console.log("Bot ready!");
+    });
+
+    client.on("message", (message) => {
+      if (message.content === "!test") {
+        message.reply("Toimii");
+      }
+    });
+}
+
+main().then(() => console.log('exit')).catch((err) => {
+  console.error('Fatal error from main function:')
+  console.error(err);
+  process.exit(1);
+});
