bf0a7fae301387d75a3d45cd89bf8c5a5c11a1e9
Tommi Haapa <tommi-haapa@hotmail.com>
2021-04-18 00:22:50 +0300

    Bot testing settings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hjello C8 World');
+import Discord from "discord.js";
+const client = new Discord.Client();
+client.login("ODMzMDY3OTM2NTY2MzQ1Nzcw.YHs8-Q.SKmcEufh0jqWGXS1GkKu1pMhqBE");
+
+client.on("ready", () => {
+    console.log("Bot ready!");
+  });
+
+  client.on("message", (message) => {
+    if (message.content === "!test") {
+      message.reply("Toimii");
+    }
+  });
